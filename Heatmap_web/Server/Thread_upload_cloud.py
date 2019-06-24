f64131908f3ae6d852ea0757d54b53ed869cda0d
huyvu0723 <33371542+huyvu0723@users.noreply.github.com>
2019-06-24 22:19:45 +0700

    Update

--- Heatmap_web/Server/Thread_upload_cloud.py
+++ Heatmap_web/Server/Thread_upload_cloud.py
@@ -3,7 +3,7 @@ from flask import Flask ,request
 from flask_socketio import SocketIO, send
 import cv2
 import datetime
-from Thread_camera import *
+import Thread_camera as camera
 from Upload_cloud import *
 
 
@@ -11,7 +11,8 @@ def uploadToCloud(socketio, id_camera, port_camera):
     print("RESTART")
     now = datetime.datetime.now()
     day = now.strftime("%Y-%m-%d")
-    camera_1 = threading.Thread(target=runCamera, args=(socketio, id_camera, port_camera,))
+    uploadFile("camera_" + str(id_camera) + "_" + day + ".avi", "./Server_data/Save_data/Camera/"+ id_camera + '/' + day + "/"+ day + ".avi", 'video/avi')
+    camera_1 = threading.Thread(target=camera.runCamera, args=(socketio, id_camera, port_camera,))
     camera_1.start()
-    uploadFile(day + ".avi", "./Server_data/Save_data/Camera/"+ id_camera + '/' + day + "/"+ day + ".avi", 'video/avi')
+    
     
