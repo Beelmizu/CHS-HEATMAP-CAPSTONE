5121ea07d63c8bafbcada61beaba37dcdd53a6a6
huyvu0723 <33371542+huyvu0723@users.noreply.github.com>
2019-08-22 11:06:29 +0700

    Update Thread_worker.py

--- RTSP_server_v6/Server/Thread_worker.py
+++ RTSP_server_v6/Server/Thread_worker.py
@@ -110,112 +110,113 @@ def detect_object(socketio, rd, id_camera):
                         if int(check_flag.decode()) == 1:
                             # Lấy ảnh từ redis và decode
                             image_base64 = rd.get(str(id_camera))
-                            # Từ base64 chuyển thành image
-                            decoded_data = base64.b64decode(image_base64.decode())
-                            np_data = np.fromstring(decoded_data,np.uint8)
-                            image = cv2.imdecode(np_data, cv2.IMREAD_UNCHANGED)
-                            height, width, channel = image.shape
-                            # Địa chỉ lấy background
-                            save_background_location = "./Server_data/Background/"+ str(width) +"x" + str(height) + ".png"
-                            if image is not None:
-                                # image = cv2.resize(image_read, (new_w, new_h))
-                                image_np_expanded = np.expand_dims(image, axis=0)
-                                image_tensor = detection_graph.get_tensor_by_name('image_tensor:0')
-                                # Each box represents a part of the image where a particular object was detected.
-                                boxes = detection_graph.get_tensor_by_name('detection_boxes:0')
-                                # Each score represent how level of confidence for each of the objects.
-                                # Score is shown on the result image, together with the class label.
-                                scores = detection_graph.get_tensor_by_name('detection_scores:0')
-                                classes = detection_graph.get_tensor_by_name('detection_classes:0')
-                                num_detections = detection_graph.get_tensor_by_name('num_detections:0')
-                                # Actual detection.
-                                (boxes, scores, classes, num_detections) = sess.run(
-                                    [boxes, scores, classes, num_detections],
-                                    feed_dict={image_tensor: image_np_expanded})
-                                # Visualization of the results of a detection.
-                                is_color_recognition_enabled = 0
-                                boxes = np.squeeze(boxes)
-                                scores = np.squeeze(scores)
-                                classes = np.squeeze(classes)
-                                # chỉ check lấy người
-                                indices = np.argwhere(classes == 1)
-                                boxes = np.squeeze(boxes[indices])
-                                scores = np.squeeze(scores[indices])
-                                classes = np.squeeze(classes[indices])
-                                counter, csv_line, the_result = vis_util.visualize_boxes_and_labels_on_image_array(1,
-                                                                                                                image,
-                                                                                                                1,
-                                                                                                                is_color_recognition_enabled,
-                                                                                                                boxes,
-                                                                                                                classes.astype(np.int32),
-                                                                                                                scores,
-                                                                                                                category_index,
-                                                                                                                targeted_objects='person',
-                                                                                                                use_normalized_coordinates=True,
-                                                                                                                line_thickness=2,
-                                                                                                                max_boxes_to_draw=None,
-                                                                                                                min_score_thresh=0.5)
-                                #record video
-                                box = np.squeeze(boxes)
-                                retval, buffer = cv2.imencode('.jpg', image)
-                                jpg_as_text = base64.b64encode(buffer)
-                                image_text = str(jpg_as_text, "utf-8")
-                                #truyền về id camera ở html
-                                # Load background và tạo mới nếu chưa có
-                                try:
-                                    background_OD = Image.open(save_background_location)
-                                except:
-                                    background_image = Image.new('RGBA', (width, height), (255,255,255,0))
-                                    background_image.save(save_background_location)
-                                    background_OD = Image.open(save_background_location)
+                            if image_base is not None:
+                                # Từ base64 chuyển thành image
+                                decoded_data = base64.b64decode(image_base64.decode())
+                                np_data = np.fromstring(decoded_data,np.uint8)
+                                image = cv2.imdecode(np_data, cv2.IMREAD_UNCHANGED)
+                                height, width, channel = image.shape
+                                # Địa chỉ lấy background
+                                save_background_location = "./Server_data/Background/"+ str(width) +"x" + str(height) + ".png"
+                                if image is not None:
+                                    # image = cv2.resize(image_read, (new_w, new_h))
+                                    image_np_expanded = np.expand_dims(image, axis=0)
+                                    image_tensor = detection_graph.get_tensor_by_name('image_tensor:0')
+                                    # Each box represents a part of the image where a particular object was detected.
+                                    boxes = detection_graph.get_tensor_by_name('detection_boxes:0')
+                                    # Each score represent how level of confidence for each of the objects.
+                                    # Score is shown on the result image, together with the class label.
+                                    scores = detection_graph.get_tensor_by_name('detection_scores:0')
+                                    classes = detection_graph.get_tensor_by_name('detection_classes:0')
+                                    num_detections = detection_graph.get_tensor_by_name('num_detections:0')
+                                    # Actual detection.
+                                    (boxes, scores, classes, num_detections) = sess.run(
+                                        [boxes, scores, classes, num_detections],
+                                        feed_dict={image_tensor: image_np_expanded})
+                                    # Visualization of the results of a detection.
+                                    is_color_recognition_enabled = 0
+                                    boxes = np.squeeze(boxes)
+                                    scores = np.squeeze(scores)
+                                    classes = np.squeeze(classes)
+                                    # chỉ check lấy người
+                                    indices = np.argwhere(classes == 1)
+                                    boxes = np.squeeze(boxes[indices])
+                                    scores = np.squeeze(scores[indices])
+                                    classes = np.squeeze(classes[indices])
+                                    counter, csv_line, the_result = vis_util.visualize_boxes_and_labels_on_image_array(1,
+                                                                                                                    image,
+                                                                                                                    1,
+                                                                                                                    is_color_recognition_enabled,
+                                                                                                                    boxes,
+                                                                                                                    classes.astype(np.int32),
+                                                                                                                    scores,
+                                                                                                                    category_index,
+                                                                                                                    targeted_objects='person',
+                                                                                                                    use_normalized_coordinates=True,
+                                                                                                                    line_thickness=2,
+                                                                                                                    max_boxes_to_draw=None,
+                                                                                                                    min_score_thresh=0.5)
+                                    #record video
+                                    box = np.squeeze(boxes)
+                                    retval, buffer = cv2.imencode('.jpg', image)
+                                    jpg_as_text = base64.b64encode(buffer)
+                                    image_text = str(jpg_as_text, "utf-8")
+                                    #truyền về id camera ở html
+                                    # Load background và tạo mới nếu chưa có
+                                    try:
+                                        background_OD = Image.open(save_background_location)
+                                    except:
+                                        background_image = Image.new('RGBA', (width, height), (255,255,255,0))
+                                        background_image.save(save_background_location)
+                                        background_OD = Image.open(save_background_location)
 
-                                dr = ImageDraw.Draw(background_OD)
-                                # Vẽ Ô vuông lên hình
-                                for i in range(len(box)):
-                                    ymin = (int(box[i,0]*height))
-                                    xmin = (int(box[i,1]*width))
-                                    ymax = (int(box[i,2]*height))
-                                    xmax = (int(box[i,3]*width))
-                                    if(ymin == 0 and xmin == 0 and ymax == 0 and xmax == 0):
-                                        break
-                                    
-                                    cor = (xmin, ymin, xmax, ymax)
-                                    dr.rectangle(cor, outline="green")
+                                    dr = ImageDraw.Draw(background_OD)
+                                    # Vẽ Ô vuông lên hình
+                                    for i in range(len(box)):
+                                        ymin = (int(box[i,0]*height))
+                                        xmin = (int(box[i,1]*width))
+                                        ymax = (int(box[i,2]*height))
+                                        xmax = (int(box[i,3]*width))
+                                        if(ymin == 0 and xmin == 0 and ymax == 0 and xmax == 0):
+                                            break
+                                        
+                                        cor = (xmin, ymin, xmax, ymax)
+                                        dr.rectangle(cor, outline="green")
 
-                                # Vẽ chữ count
-                                try:
-                                    count_number = int(''.join(filter(str.isdigit, the_result)))
-                                except Exception as e:
-                                    count_number = 0
-                                    pass
-                                #font chữ load ở bên trên
-                                dr.text((250, 10),"Person: " + str(count_number),(0,255,0), font=font)
-                                # Lấy kết quả của nhận dạng gender vs age
-                                face_re = rd.get(str(id_camera) + "_FR").decode()
-                                if face_re == None:
-                                    face_re = "Loading..."
-                                dr.text((250, 30),face_re,(0,255,0), font=font_face_re)
-                                heatmap_time = datetime.datetime.now()
-                                # print(upload_time)
-                                if heatmap_time > heatmap_run_time:
-                                    #Chạy heatmap sau khi chạy xong retake_heatmap_count frame
-                                    heatmap_run_time = heatmap_time + datetime.timedelta(minutes=1)
-                                    # setCount(count_number, id_camera)
-                                    current_time = datetime.datetime.now()
-                                    # print("HHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHH")
-                                    # print(now)
+                                    # Vẽ chữ count
+                                    try:
+                                        count_number = int(''.join(filter(str.isdigit, the_result)))
+                                    except Exception as e:
+                                        count_number = 0
+                                        pass
+                                    #font chữ load ở bên trên
+                                    dr.text((250, 10),"Person: " + str(count_number),(0,255,0), font=font)
+                                    # Lấy kết quả của nhận dạng gender vs age
+                                    face_re = rd.get(str(id_camera) + "_FR").decode()
+                                    if face_re == None:
+                                        face_re = "Loading..."
+                                    dr.text((250, 30),face_re,(0,255,0), font=font_face_re)
+                                    heatmap_time = datetime.datetime.now()
                                     # print(upload_time)
-                                    if current_time > upload_time:
-                                        upload_time = upload_time + datetime.timedelta(days=1)
-                                        upload_time = upload_time.replace(hour=0, minute=0, second=0)
-                                        matrix_heatmap = []
-                                    heatmap = threading.Thread(target=thread_hm.draw_heatmap, args=(socketio, rd, id_camera, matrix_heatmap, box, width, height, heatmap_time, count_number,))
-                                    heatmap.start()
-                                # Chuyển thành base64 để đẩy lên redis
-                                buffered = BytesIO()
-                                background_OD.save(buffered, format="PNG")
-                                img_str = base64.b64encode(buffered.getvalue())
-                                rd.set(str(id_camera) + "_OD", img_str)
+                                    if heatmap_time > heatmap_run_time:
+                                        #Chạy heatmap sau khi chạy xong retake_heatmap_count frame
+                                        heatmap_run_time = heatmap_time + datetime.timedelta(minutes=1)
+                                        # setCount(count_number, id_camera)
+                                        current_time = datetime.datetime.now()
+                                        # print("HHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHH")
+                                        # print(now)
+                                        # print(upload_time)
+                                        if current_time > upload_time:
+                                            upload_time = upload_time + datetime.timedelta(days=1)
+                                            upload_time = upload_time.replace(hour=0, minute=0, second=0)
+                                            matrix_heatmap = []
+                                        heatmap = threading.Thread(target=thread_hm.draw_heatmap, args=(socketio, rd, id_camera, matrix_heatmap, box, width, height, heatmap_time, count_number,))
+                                        heatmap.start()
+                                    # Chuyển thành base64 để đẩy lên redis
+                                    buffered = BytesIO()
+                                    background_OD.save(buffered, format="PNG")
+                                    img_str = base64.b64encode(buffered.getvalue())
+                                    rd.set(str(id_camera) + "_OD", img_str)
                     else:
                         break    
                 except Exception as e:
