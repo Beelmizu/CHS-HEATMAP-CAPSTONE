5672f2b9e14fc32b9ef236c0bb801cda4b27ca85
huyvu0723 <33371542+huyvu0723@users.noreply.github.com>
2019-09-15 12:30:30 +0700

    delete v3

--- Web/V3.0/WebAdmin/src/app/models/company.model.ts
+++
@@ -1,9 +0,0 @@
-export class Company  {
-    public id: number;
-    public name: String;
-    public address: String;
-    public createdDate: String;
-    public updatedDate: String;
-    public status: String;
-    public updatedBy: String;
-}
